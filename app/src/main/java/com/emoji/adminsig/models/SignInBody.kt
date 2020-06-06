2de2780cf40af0775983737cbf73a72dc3a30570
EmoG <gramdhani66@gmail.com>
2020-06-06 21:38:20 +0800

    + login session

--- app/src/main/java/com/emoji/adminsig/models/SignInBody.kt
+++ app/src/main/java/com/emoji/adminsig/models/SignInBody.kt
@@ -1,6 +1,7 @@
 package com.emoji.adminsig.models
 
 data class SignInBody(
+    val id_admin: String,
     val username: String,
-    val password: String,
-    val id_admin: String)
+    val password: String
+    )
